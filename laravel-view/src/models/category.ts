b289130a98b589fab380939de1068316f2fd8b28
Alex Ivanov <oleksii120@gmail.com>
2023-06-25 17:40:34 +0300

    Added yup, changed creation form Changed formik to use components instead of functions

--- laravel-view/src/models/category.ts
+++ laravel-view/src/models/category.ts
@@ -1,6 +1,11 @@
-export default interface ICategoryItem {
-    get id(): number | undefined;
-    get name(): string;
-    get image(): string;
-    get description(): string;
-}
+import { object, string, number, InferType } from 'yup';
+
+export const categorySchema = object({
+    id: number().nullable(),
+    name: string().min(3, "Too short").max(100, 'Too long').required(),
+    image: string().max(400, 'Shorten the path').required(),
+    description: string().max(500, 'Too long').nullable(),
+});
+
+type ICategoryItem = InferType<typeof categorySchema>;
+export default ICategoryItem;
