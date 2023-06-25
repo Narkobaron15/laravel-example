07213b104bb018614f79569382a7eb87155e7c85
Narkobaron15 <oleksii120@gmail.com>
2023-06-25 22:03:58 +0300

    Added displaying of error texts Removed unnecessary files Added comments

--- laravel-view/src/components/category/create/createcategory.tsx
+++ laravel-view/src/components/category/create/createcategory.tsx
@@ -1,6 +1,6 @@
 import React from "react"
 import { useNavigate } from "react-router-dom";
-import { useFormik, Formik, Form, Field } from "formik";
+import { Formik, Form, Field } from "formik";
 
 import ICategoryItem, { categorySchema } from "../../../models/category";
 import http_common from "../../../http_common";
@@ -13,68 +13,91 @@ const emptyCategory: ICategoryItem = {
 }
 
 export default function CreateCategory() {
+    // used for redirecting after successful form submit
     const navigate = useNavigate();
 
+    // the logic of submit button on formik form
     const formikSubmit = async (val: ICategoryItem) => {
         try {
             const validatedVal = await categorySchema.validate(val);
             await http_common.post("api/categories/create", validatedVal);
             navigate("/all");
         }
-        catch (err) {
-            console.error('Server side error. Details: ' + err);
+        catch (err: any) {
+            console.error('Server side error. Details: ' + err.message);
         }
     }
 
     // https://formik.org/docs/guides/validation
-    // TODO: Add validation errors, test them
 
+    // Formik component syntax
     return (
         <Formik initialValues={emptyCategory} validationSchema={categorySchema} onSubmit={formikSubmit}>
-          <Form className="mx-auto">
-            <div className="form-group">
-              <div className="md:w-1/3">
-                <label htmlFor="name">Назва</label>
-              </div>
-              <div className="md:w-2/3">
-                <Field
-                  id="name"
-                  name="name"
-                  type="text"
-                  placeholder="Введіть назву..."
-                />
-              </div>
-            </div>
-            <div className="form-group">
-              <div className="md:w-1/3">
-                <label htmlFor="image">Зображення</label>
-              </div>
-              <div className="md:w-2/3">
-                <Field
-                  id="image"
-                  name="image"
-                  type="text"
-                  placeholder="Введіть адресу зображення..."
-                />
-              </div>
-            </div>
-            <div className="form-group">
-              <div className="md:w-1/3">
-                <label htmlFor="description">Опис</label>
-              </div>
-              <div className="md:w-2/3">
-                <Field
-                  id="description"
-                  name="description"
-                  type="text"
-                  placeholder="Введіть опис..."
-                />
-              </div>
-            </div>
-            <div className="flex justify-center">
-              <button type="submit">Додати</button>
-            </div>
-          </Form>
+            {({ errors, touched }) => (
+                <Form className="mx-auto">
+                    <div className="form-group">
+                        <div className="md:w-1/3">
+                            <label htmlFor="name">Назва</label>
+                        </div>
+                        <div className="md:w-2/3">
+                            <Field
+                                id="name"
+                                name="name"
+                                type="text"
+                                placeholder="Введіть назву..."
+                            />
+                        </div>
+                    </div>
+                    {/* 
+                      * if any errors were registered,
+                      * the error message will be shown
+                      */}
+                    {
+                        errors.name && touched.name
+                            ? <div className="error-text">{errors.name}</div>
+                            : null
+                    }
+                    <div className="form-group">
+                        <div className="md:w-1/3">
+                            <label htmlFor="image">Зображення</label>
+                        </div>
+                        <div className="md:w-2/3">
+                            <Field
+                                id="image"
+                                name="image"
+                                type="text"
+                                placeholder="Введіть адресу зображення..."
+                            />
+                        </div>
+                    </div>
+                    {
+                        errors.image && touched.image
+                            ? <div className="error-text">{errors.image}</div>
+                            : null
+                    }
+                    <div className="form-group">
+                        <div className="md:w-1/3">
+                            <label htmlFor="description">Опис</label>
+                        </div>
+                        <div className="md:w-2/3">
+                            <Field
+                                id="description"
+                                name="description"
+                                type="text"
+                                placeholder="Введіть опис..."
+                            />
+                        </div>
+                    </div>
+                    {
+                        errors.description && touched.description
+                            ? <div className="error-text">{errors.description}</div>
+                            : null
+                    }
+                    <div className="flex justify-center">
+                        <button type="submit">Додати</button>
+                    </div>
+                </Form>
+            )}
         </Formik>
-      );
+    );
 }
--- laravel-view/src/http_common.ts
+++ laravel-view/src/http_common.ts
@@ -1,6 +1,12 @@
 import axios from "axios";
 import APP_ENV from "./env/app_env";
 
+/*
+ * axios alias for requests, 
+ * that will be taking only URI and send a request,
+ * constructing full path, to the server (baseURL), 
+ * with provided defaults (like headers)
+ */
 const http_common = axios.create({
     baseURL: APP_ENV.BASE_URL,
     headers: {
