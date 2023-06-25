9968959e3c462c0d0f7c378f50a52f9d9f590467
Alex Ivanov <oleksii120@gmail.com>
2023-06-25 17:40:34 +0300

    Added yup, changed creation form Changed formik to use components instead of functions
    #1 Test if these changes work

--- laravel-view/src/components/category/create/createcategory.tsx
+++ laravel-view/src/components/category/create/createcategory.tsx
@@ -1,9 +1,8 @@
 import React from "react"
 import { useNavigate } from "react-router-dom";
-import { useFormik } from "formik";
+import { useFormik, Formik, Form, Field } from "formik";
 
-import APP_ENV from "../../../env/app_env";
-import ICategoryItem from "../../../models/category";
+import ICategoryItem, { categorySchema } from "../../../models/category";
 import http_common from "../../../http_common";
 
 const emptyCategory: ICategoryItem = {
@@ -15,65 +14,67 @@ const emptyCategory: ICategoryItem = {
 
 export default function CreateCategory() {
     const navigate = useNavigate();
-    const formikSubmit = async (value: ICategoryItem) => {
+
+    const formikSubmit = async (val: ICategoryItem) => {
         try {
-            await http_common.post("api/categories/create", values);
+            const validatedVal = await categorySchema.validate(val);
+            await http_common.post("api/categories/create", validatedVal);
             navigate("/all");
         }
         catch (err) {
             console.error('Server side error. Details: ' + err);
         }
     }
-    const formik = useFormik({
-        initialValues: emptyCategory,
-        onSubmit: formikSubmit,
-    });
-    const { values, handleSubmit, handleChange } = formik;
+
+    // https://formik.org/docs/guides/validation
+    // TODO: Add validation errors, test them
 
     return (
-        <>
-            <h1>Додати нову категорію</h1>
-            <form onSubmit={handleSubmit} className="mx-auto">
-                <div className="form-group">
-                    <div className="md:w-1/3">
-                        <label htmlFor="name">
-                            Назва
-                        </label>
-                    </div>
-                    <div className="md:w-2/3">
-                        <input id="name" name="name" type="text" placeholder="Введіть назву..."
-                            value={values.name} onChange={handleChange} />
-                    </div>
-                </div>
-                <div className="form-group">
-                    <div className="md:w-1/3">
-                        <label htmlFor="image">
-                            Зображення
-                        </label>
-                    </div>
-                    <div className="md:w-2/3">
-                        <input id="image" name="image" type="text" placeholder="Введіть адресу зображення..."
-                            value={values.image} onChange={handleChange} />
-                    </div>
-                </div>
-                <div className="form-group">
-                    <div className="md:w-1/3">
-                        <label htmlFor="description">
-                            Опис
-                        </label>
-                    </div>
-                    <div className="md:w-2/3">
-                        <input id="description" name="description" type="text" placeholder="Введіть опис..."
-                            value={values.description} onChange={handleChange} />
-                    </div>
-                </div>
-
-                <div className="flex justify-center">
-                    <button type="submit">
-                        Додати
-                    </button>
-                </div>
-            </form>
-        </>
-    )
+        <Formik initialValues={emptyCategory} validationSchema={categorySchema} onSubmit={formikSubmit}>
+          <Form className="mx-auto">
+            <div className="form-group">
+              <div className="md:w-1/3">
+                <label htmlFor="name">Назва</label>
+              </div>
+              <div className="md:w-2/3">
+                <Field
+                  id="name"
+                  name="name"
+                  type="text"
+                  placeholder="Введіть назву..."
+                />
+              </div>
+            </div>
+            <div className="form-group">
+              <div className="md:w-1/3">
+                <label htmlFor="image">Зображення</label>
+              </div>
+              <div className="md:w-2/3">
+                <Field
+                  id="image"
+                  name="image"
+                  type="text"
+                  placeholder="Введіть адресу зображення..."
+                />
+              </div>
+            </div>
+            <div className="form-group">
+              <div className="md:w-1/3">
+                <label htmlFor="description">Опис</label>
+              </div>
+              <div className="md:w-2/3">
+                <Field
+                  id="description"
+                  name="description"
+                  type="text"
+                  placeholder="Введіть опис..."
+                />
+              </div>
+            </div>
+            <div className="flex justify-center">
+              <button type="submit">Додати</button>
+            </div>
+          </Form>
+        </Formik>
+      );
 }
